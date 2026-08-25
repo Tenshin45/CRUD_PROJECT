e61609c25dbb282f1d6b0abec3cdac1208f57177
Tenshin45 <enkatakanatenshi@gmail.com>
2026-08-25 10:23:50 +0300

    PROJECT

--- PROJECT/APP/forms.py
+++ PROJECT/APP/forms.py
@@ -17,7 +17,6 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email address'}))
     first_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter first name'}))
     last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter last name'}))
-
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
