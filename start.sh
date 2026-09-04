3fc600adda8b812e580601c7cffb6dd7657fc076
Tenshin45 <emmanuelbarhayigaschool@gmail.com>
2026-09-04 00:20:24 -0700

    Web Tech

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ set -e
 
 cd PROJECT
 python manage.py migrate --noinput
-python manage.py create_deploy_superuser || true
+python manage.py createsuperuser --noinput || true
 python manage.py collectstatic --noinput
 exec gunicorn PROJECT.wsgi --bind 0.0.0.0:${PORT:-8000}
